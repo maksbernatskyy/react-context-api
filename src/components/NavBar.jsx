f0c6e87d6a50c2a21e278271fca8056b79250c03
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-18 13:28:32 +0100

    add condition where onClick we change the button of the navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,18 @@
 {/* Import Link and NavLink */}
 import { Link, NavLink } from "react-router-dom"
 
+// Import useContext
+import { useContext } from "react"
+// Import Budget Context
+import BudgetContext from "../contexts/BudgetContext"
+
 export default function NavBar() {
+    const {budgetMode, setBudgetMode} = useContext(BudgetContext)
+
+    function handleClick() {
+        budgetMode ? setBudgetMode(false) : setBudgetMode(true)
+        console.log(budgetMode)
+    }
 
     return (
         <>
@@ -9,7 +20,7 @@ export default function NavBar() {
                 <Link className="text-decoration-none text-secondary me-3 fw-bold" to="/">Home Page</Link>
                 <NavLink className="text-decoration-none text-secondary me-3 fw-bold" to="/ChiSiamo">Chi Siamo</NavLink>
                 <NavLink className="text-decoration-none text-secondary fw-bold" to="/Products">Prodotti</NavLink>
-                <button className=" mx-2 btn btn-primary">
+                <button className=" mx-2 btn btn-primary" onClick={handleClick}>
                     Budget Mode
                 </button>
             </nav>
