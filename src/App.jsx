e4adea63028ab76c9b82495de514f9a4cbfb29b9
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-18 12:33:35 +0100

    import in app.jsx the budget context

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,26 @@ import ChiSiamo from "./pages/ChiSiamo"
 import Products from "./pages/Products"
 import SingleProduct from "./pages/SingleProduct"
 
+// Budget context
+import BudgetContext from "./contexts/BudgetContext"
+
 function App() {
   
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/ChiSiamo" element={<ChiSiamo />} />
-            <Route path="/Products" element={<Products />} />
-            <Route path="/Products/:id" element={<SingleProduct />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <BudgetContext.Provider value={{}}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/ChiSiamo" element={<ChiSiamo />} />
+              <Route path="/Products" element={<Products />} />
+              <Route path="/Products/:id" element={<SingleProduct />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BudgetContext.Provider>
     </>
   )
 }
