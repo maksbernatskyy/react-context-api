b7352d592f4c36ac15aaf056d7987a89c0760d3b
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-18 12:51:39 +0100

    add button budget mode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,6 +9,9 @@ export default function NavBar() {
                 <Link className="text-decoration-none text-secondary me-3 fw-bold" to="/">Home Page</Link>
                 <NavLink className="text-decoration-none text-secondary me-3 fw-bold" to="/ChiSiamo">Chi Siamo</NavLink>
                 <NavLink className="text-decoration-none text-secondary fw-bold" to="/Products">Prodotti</NavLink>
+                <button className=" mx-2 btn btn-primary">
+                    Budget Mode
+                </button>
             </nav>
         </>
     )
