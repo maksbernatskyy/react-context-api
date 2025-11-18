e526fafae406562b276867bf44f860fd3abc99ad
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-18 17:15:49 +0100

    fix singleProduct

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,29 +1,34 @@
 import { useParams } from "react-router-dom"
-import cards from "../assets/img/cards"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export default function SingleProduct() {
 
-    <cards />
-
-     
+    const[singleCard, setSingleCard] = useState({})
 
     {/* Params for id */}
     const { id } = useParams()
 
-    {/* Find the product with id */}
-    const card = cards.find((thisCard) => thisCard.id === Number(id))
+    {/* Call API for the single card */}
+    function fetchCard() {
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+        .then((res) => setSingleCard(res.data))
+        .catch((err) => console.error('Errore:', err))
+    }
+
+    useEffect(fetchCard, [])
 
     return (
         <>
         <div className="container mt-4">
             <div id="singleCard" className="card p-3">
                 <div>
-                    <img className="card-img-top" src={card.image} alt="" />
+                    <img className="card-img-top" src={singleCard.image} alt="" />
                 </div>
                 <div className="card-body">
-                    <span className="text-uppercase fw-bold d-block">{card.title}</span>
-                    <span className="d-block"><strong>Price:</strong> "{card.price} €"</span>
-                    <span className="d-block"><strong>Category:</strong> "{card.category}"</span>
+                    <span className="text-uppercase fw-bold d-block">{singleCard.title}</span>
+                    <span className="d-block"><strong>Price:</strong> "{singleCard.price} €"</span>
+                    <span className="d-block"><strong>Category:</strong> "{singleCard.category}"</span>
                 </div>
             </div>
         </div>
