3380accfc85c89245c71164f12b4d8bc56e07318
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-18 12:48:43 +0100

    add component nav bar in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import logo from "../assets/img/logo.jpeg"
-
-{/* Import Link and NavLink */}
-import { Link, NavLink } from "react-router-dom"
+import NavBar from "./NavBar"
 
 export default function Header() {
 
@@ -13,11 +11,7 @@ export default function Header() {
                     <img src={logo} alt="Logo" />
                 </div>
                 <div>
-                    <nav className="navbar navbar-expand-lg">
-                        <Link className="text-decoration-none text-secondary me-3 fw-bold" to="/">Home Page</Link>
-                        <NavLink className="text-decoration-none text-secondary me-3 fw-bold" to="/ChiSiamo">Chi Siamo</NavLink>
-                        <NavLink className="text-decoration-none text-secondary fw-bold" to="/Products">Prodotti</NavLink>
-                    </nav>
+                    <NavBar />
                 </div>
             </div>
           </header>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,15 @@
+{/* Import Link and NavLink */}
+import { Link, NavLink } from "react-router-dom"
+
 export default function NavBar() {
 
     return (
         <>
-          
+          <nav className="navbar navbar-expand-lg">
+                <Link className="text-decoration-none text-secondary me-3 fw-bold" to="/">Home Page</Link>
+                <NavLink className="text-decoration-none text-secondary me-3 fw-bold" to="/ChiSiamo">Chi Siamo</NavLink>
+                <NavLink className="text-decoration-none text-secondary fw-bold" to="/Products">Prodotti</NavLink>
+            </nav>
         </>
     )
 }
